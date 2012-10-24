8a64c12bd2b2d1ee07ef6af41106788f28c341a5
Christopher Gore <theOutLiar@gmail.com>
2012-10-24 19:54:43 -0400

    first pass at extracting style attribute data from html

---
+++ inline2stylesheet.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+from HTMLParser import HTMLParser
+class inline2stylesheet(HTMLParser):
+    stylesDict = {}
+    tagNest = []
+    def handle_starttag(self, tag, attrs):
+        self.tagNest.append(tag)
+        if(len(attrs) <= 0): return
+        attrDict = dict(attrs)
+        if(not attrDict.has_key('style')): return
+        styleString = attrDict['style']
+        styleList = [rule.strip() for rule in styleString.split(';') if rule.strip()!='']
+        styleList.sort();
+        import pdb; pdb.set_trace();
+    def handle_endtag(self, tag):
+        self.tagNest.pop();
+if __name__=="__main__":
+    f = file('index.html')
+    p = inline2stylesheet()
+    p.feed(f.read());
