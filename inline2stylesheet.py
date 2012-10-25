56a07b5f9963b833b3520db96202c6f34a72d00b
Christopher Gore <theOutLiar@gmail.com>
2012-10-24 21:07:20 -0400

    prints each unique style in a file

--- inline2stylesheet.py
+++ inline2stylesheet.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 from HTMLParser import HTMLParser
 class inline2stylesheet(HTMLParser):
+    """this class extends the HTMLParser to extract the css from the style attributes of html elements, and output them in a generated css file"""
     stylesDict = {}
     tagNest = []
     def handle_starttag(self, tag, attrs):
@@ -9,11 +10,20 @@ class inline2stylesheet(HTMLParser):
         attrDict = dict(attrs)
         if(not attrDict.has_key('style')): return
         styleString = attrDict['style']
-        styleList = [rule.strip() for rule in styleString.split(';') if rule.strip()!='']
-        styleList.sort();
+        styleList = [rule.strip()+";" for rule in styleString.split(';') if rule.strip()!='']
+        styleList.sort()
+        styleKey = str().join(styleList)
+        if(not self.stylesDict.has_key(styleKey)):
+            self.stylesDict[styleKey] = []
+        if(not self.stylesDict[styleKey].count(styleString)):
+            self.stylesDict[styleKey].append(styleString);
     def handle_endtag(self, tag):
         self.tagNest.pop();
+    def printStyles(self):
+        for key in self.stylesDict.keys():
+            print key
 if __name__=="__main__":
     f = file('index.html')
     p = inline2stylesheet()
-    p.feed(f.read());
+    p.feed(f.read())
+    p.printStyles()
